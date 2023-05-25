d77f80356222d61c93880ee21e0781eb091b75bb
syzhao53 <syzhao@sas.upenn.edu>
2023-05-25 15:31:02 -0400

    text area + section adding setup

--- cookbooked/components/EditingSidebar.js
+++ cookbooked/components/EditingSidebar.js
@@ -5,41 +5,51 @@ import { useState } from 'react'
 
 const EditingSidebar = ({title, setTitle}) => {
   const [editingSection, setEditingSection] = useState('intro')
+  const [sections, setSections] = useState([title])
+
+  const addSection = () => {
+    sections.push('Section')
+    alert('section added' + sections.length)
+  }
 
   return (
     <div
       className="w-1/5 pt-7 border-r-2 border-light_gray h-[calc(100vh-64px)] hidden md:block"
       style={{ position: 'fixed' }}
     >
-                <div
-              key="section"
-              className={`text-2xl cursor-pointer py-3 ${
-                editingSection === 'intro'
-                  ? 'bg-light_purple'
-                  : 'hover:bg-light_purple/50'
-              }`}
-              onClick={() => setEditingSection('')}
-            >
-              <div
-                className={`flex items-center ml-9 ${
-                  editingSection === 'intro' && 'font-medium'
-                }`}
-              >
-                {/* {sectionIdx !== 0 && (
-                  <CircleNumber number={sectionIdx} />
-                )} */}
-                {<span className="text-med_gray">{title == 'Recipe Title' ? 'Recipe Overview' : title}</span>}
-              </div>
-              {/* {sectionIdx !== 0 && (
-                <span className="pl-16 text-base text-gray">
-                  {
-                      duration[sectionName].hours > 0
-                      ? duration[sectionName].hours + " hr " + duration[sectionName].minutes
-                      : duration[sectionName].minutes + " min"
-                    }
-                </span>
-              )} */}
-            </div>
+      {sections.map((sec, idx) => (
+         <div
+         key="section"
+         className={`text-2xl cursor-pointer py-3 ${
+           editingSection === 'intro'
+             ? 'bg-light_purple'
+             : 'hover:bg-light_purple/50'
+         }`}
+         onClick={() => setEditingSection('')}
+       >
+         <div
+           className={`flex items-center ml-9 pr-5 ${
+             editingSection === 'intro' && 'font-medium'
+           }`}
+         >
+           {/* {sectionIdx !== 0 && (
+             <CircleNumber number={sectionIdx} />
+           )} */}
+           {<span className={`${title !== 'Recipe Title' ? 'text-black' : 'text-med_gray'}`} >
+           {title == 'Recipe Title' ? 'Recipe Overview' : title}</span>}
+         </div>
+         {/* {sectionIdx !== 0 && (
+           <span className="pl-16 text-base text-gray">
+             {
+                 duration[sectionName].hours > 0
+                 ? duration[sectionName].hours + " hr " + duration[sectionName].minutes
+                 : duration[sectionName].minutes + " min"
+               }
+           </span>
+         )} */}
+       </div>
+      ))}
+      
       {/* {Object.entries(allSections).map(
         ([sectionKey, sectionName], sectionIdx) => {
           return (
@@ -75,6 +85,9 @@ const EditingSidebar = ({title, setTitle}) => {
           )
         }
       )} */}
+              <button onClick={() => addSection()} className="bg-green-300 mt-6">
+                Add Section
+              </button>
     </div>
   )
 }
--- cookbooked/components/styles/Input.js
+++ cookbooked/components/styles/Input.js
@@ -1,3 +1,5 @@
+import TextareaAutosize from 'react-textarea-autosize';
+
 const textSize = (text) => {
     switch (text) {
       case 'large':
@@ -11,13 +13,13 @@ const textSize = (text) => {
 
 export const Input = ({ prop, setProp, defaultText, textType }) => {
     return (
-        <input type="text" value={prop}
+        <TextareaAutosize minRows={1} type="text" value={prop}
             onChange={(e) => {setProp(e.target.value)}}
             onFocus={() => {prop == defaultText && setProp('')}}
             onBlur={() => {prop == '' && setProp(defaultText)}}
             className={`${prop !== defaultText ? 'text-black' : 'text-med_gray'} 
             ${textSize(textType)}
-            border-0 focus:outline-none font-medium w-full`}
+            border-0 focus:outline-none font-medium w-full resize-none overflow-visible bg-bg_white`}
         />
     )
   }
